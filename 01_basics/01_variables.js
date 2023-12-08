b6a4b872ad59e4ba5650a412cbb0c3d1f2620c45
Dr. Baisakhi Mal <131129333+bmalphys@users.noreply.github.com>
2023-12-08 19:51:44 +0000

    variables discussion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12233
+let accountEmail = "baisakhi@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+accountEmail = "bsk@bsk.com"
+accountPassword = "9876"
+accountCity = "Kolkata"
+
+console.log(accountId);
